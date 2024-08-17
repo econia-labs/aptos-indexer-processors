1a5bf0256f4e8118fd8b8e233bb7c2a5c91389f3
matt <xbtmatt@gmail.com>
2024-08-17 00:52:30 -0700

    Update comment on concurrent processing, remove unnecessary 0x7 filter

--- rust/start.sh
+++ rust/start.sh
@@ -10,15 +10,9 @@ server_config:
   indexer_grpc_http2_ping_timeout_in_secs: 10
   auth_token: $GRPC_AUTH_TOKEN
   starting_version: $STARTING_VERSION
+  # BE CAREFUL CHANGING THIS VALUE. Concurrent processing may result in undefined behavior.
   number_concurrent_processing_tasks: 1
   transaction_filter:
-    # Only allow transactions from these contract addresses
-    # focus_contract_addresses:
-    #   - "0x0"
-    # Skip transactions from these sender addresses
-    skip_sender_addresses:
-      - "0x07"
-    # Skip all transactions that aren't user transactions
     focus_user_transactions: true" > /app/config.yaml
 
 /usr/local/bin/processor -c /app/config.yaml
