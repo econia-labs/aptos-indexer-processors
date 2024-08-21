7676b948994f7553b4e4b41893acfb625c773216
matt <xbtmatt@gmail.com>
2024-08-21 15:32:49 -0700

    Change `mkts_out_of_bonding_curve_idx` to `mkts_post_bonding_curve_idx`

--- rust/processor/src/db/postgres/migrations/2024-08-13-014340_add_latest_market_state/up.sql
+++ rust/processor/src/db/postgres/migrations/2024-08-13-014340_add_latest_market_state/up.sql
@@ -47,7 +47,7 @@ CREATE TABLE market_latest_state_event (
   PRIMARY KEY (market_id)
 );
 
-CREATE INDEX mkts_out_of_bonding_curve_idx
+CREATE INDEX mkts_post_bonding_curve_idx
 ON market_latest_state_event (market_id)
 WHERE in_bonding_curve = FALSE;
 
