303c30288794e92412d1c5445c4323267e146fb5
Bogdan Crisan <bogdan@crbl.studio>
2025-02-18 16:48:52 +0100

    feat(ECO-2864): Consolidate arena migrations in one migration

--- rust/processor/src/db/postgres/migrations/2025-01-08-171729_emojicoin_arena/down.sql
+++ rust/processor/src/db/postgres/migrations/2025-01-08-171729_emojicoin_arena/down.sql
@@ -1,34 +1,11 @@
--- This file should undo anything in `up.sql`
-
-DROP INDEX latest_swap_by_market_address;
-
-DROP TRIGGER update_position_enter_trigger ON arena_enter_events;
-DROP TRIGGER update_position_exit_trigger ON arena_exit_events;
-DROP TRIGGER update_position_swap_trigger ON arena_swap_events;
-DROP TRIGGER save_leaderboard_history_trigger ON arena_melee_events;
-
-DROP FUNCTION update_position_enter;
-DROP FUNCTION update_position_exit;
-DROP FUNCTION update_position_swap;
-DROP FUNCTION save_leaderboard_history;
-
-DROP TRIGGER update_arena_info_enter_trigger ON arena_enter_events;
-DROP TRIGGER update_arena_info_exit_trigger ON arena_exit_events;
-DROP TRIGGER update_arena_info_swap_trigger ON arena_swap_events;
-DROP TRIGGER create_melee_info_trigger ON arena_melee_events;
-
-DROP FUNCTION update_arena_info_enter;
-DROP FUNCTION update_arena_info_exit;
-DROP FUNCTION update_arena_info_swap;
-DROP FUNCTION create_melee_info;
-
+DROP VIEW arena_leaderboard_history_with_arena_info;
 DROP VIEW arena_leaderboard;
 
-DROP TABLE arena_melee_events;
-DROP TABLE arena_enter_events;
-DROP TABLE arena_exit_events;
-DROP TABLE arena_swap_events;
-DROP TABLE arena_vault_balance_update_events;
-DROP TABLE arena_leaderboard_history;
-DROP TABLE arena_positions;
-DROP TABLE arena_info;
+DROP TABLE arena_melee_events ;
+DROP TABLE arena_enter_events ;
+DROP TABLE arena_exit_events ;
+DROP TABLE arena_swap_events ;
+DROP TABLE arena_vault_balance_update_events ;
+DROP TABLE arena_position ;
+DROP TABLE arena_leaderboard_history ;
+DROP TABLE arena_info ;
