3150956c525b036bc7dbb365234d03165611de66
Bogdan Crisan <bogdan@crbl.studio>
2025-02-18 16:48:52 +0100

    feat(ECO-2864): Consolidate arena migrations in one migration

--- rust/processor/src/db/postgres/schema.rs
+++ rust/processor/src/db/postgres/schema.rs
@@ -134,6 +134,7 @@ diesel::table! {
         tap_out_fee -> Numeric,
         emojicoin_0_proceeds -> Numeric,
         emojicoin_1_proceeds -> Numeric,
+        apt_proceeds -> Numeric,
         emojicoin_0_exchange_rate_base -> Numeric,
         emojicoin_0_exchange_rate_quote -> Numeric,
         emojicoin_1_exchange_rate_base -> Numeric,
@@ -146,17 +147,18 @@ diesel::table! {
         melee_id -> Numeric,
         volume -> Numeric,
         rewards_remaining -> Numeric,
-        apt_locked -> Numeric,
+        emojicoin_0_locked -> Numeric,
+        emojicoin_1_locked -> Numeric,
         emojicoin_0_market_address -> Nullable<Text>,
         emojicoin_1_market_address -> Nullable<Text>,
-        start_time -> Nullable<Timestamp>,
-        duration -> Nullable<Numeric>,
-        max_match_percentage -> Nullable<Numeric>,
-        max_match_amount -> Nullable<Numeric>,
         emojicoin_0_symbols -> Nullable<Array<Nullable<Text>>>,
         emojicoin_1_symbols -> Nullable<Array<Nullable<Text>>>,
         emojicoin_0_market_id -> Nullable<Numeric>,
         emojicoin_1_market_id -> Nullable<Numeric>,
+        start_time -> Nullable<Timestamp>,
+        duration -> Nullable<Numeric>,
+        max_match_percentage -> Nullable<Numeric>,
+        max_match_amount -> Nullable<Numeric>,
     }
 }
 
@@ -166,10 +168,11 @@ diesel::table! {
         melee_id -> Numeric,
         profits -> Numeric,
         losses -> Numeric,
-        last_exit -> Nullable<Text>,
         emojicoin_0_balance -> Numeric,
         emojicoin_1_balance -> Numeric,
         exited -> Bool,
+        last_exit_0 -> Nullable<Bool>,
+        withdrawals -> Numeric,
     }
 }
 
@@ -195,7 +198,7 @@ diesel::table! {
 }
 
 diesel::table! {
-    arena_positions (user, melee_id) {
+    arena_position (user, melee_id) {
         user -> Text,
         melee_id -> Numeric,
         open -> Bool,
@@ -203,8 +206,8 @@ diesel::table! {
         emojicoin_1_balance -> Numeric,
         withdrawals -> Numeric,
         deposits -> Numeric,
-        last_exit -> Nullable<Text>,
         match_amount -> Numeric,
+        last_exit_0 -> Nullable<Bool>,
     }
 }
 
@@ -1819,7 +1822,7 @@ diesel::allow_tables_to_appear_in_same_query!(
     arena_info,
     arena_leaderboard_history,
     arena_melee_events,
-    arena_positions,
+    arena_position,
     arena_swap_events,
     arena_vault_balance_update_events,
     block_metadata_transactions,
