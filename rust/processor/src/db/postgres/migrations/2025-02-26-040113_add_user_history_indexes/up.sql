9f83b143ebaa6b56e7c2047d6ab9e9e1c7c4e46b
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-02-28 14:12:09 -0800

    chore: Make queries more efficient through indexes with INCLUDEs

--- rust/processor/src/db/postgres/migrations/2025-02-26-040113_add_user_history_indexes/up.sql
+++ rust/processor/src/db/postgres/migrations/2025-02-26-040113_add_user_history_indexes/up.sql
@@ -5,7 +5,8 @@ ALTER TABLE chat_events
 ALTER TABLE market_registration_events
     ADD COLUMN event_index BIGINT NOT NULL;
     
---------------------------------------------------------------------------------
+-- Why use "sender" and not the event emitted address? Explained below.
+--
 -- For these indexes, "sender" is used as the representation of a user instead
 -- of the emitted fields that represent the interacting account's address;
 -- i.e., the "swapper", "user", "provider", and "registrant". 
@@ -26,13 +27,117 @@ ALTER TABLE market_registration_events
 -- However, the difference between "sender" and the corresponding event field
 -- falls under the scope of application logic and is thus left to be dealt
 -- with there, not here.
+
 --------------------------------------------------------------------------------
 
--- For querying a user's trade history for a single market.
-CREATE INDEX sender_mkt_swap_hstry_idx ON swap_events (sender, market_id, transaction_version);
+-- Include all the row data in each index to make the queries much more efficient.
+-- The difference between the two indexes for each table is just that one of them
+-- INCLUDEs `market_id` and the other indexes on it in the composite index, to
+-- facilitate all markets vs per market queries.
 
--- For querying all of a user's activity of a certain type.
-CREATE INDEX sender_all_swap_hstry_idx ON swap_events (sender, transaction_version);
-CREATE INDEX sender_all_chat_hstry_idx ON chat_events (sender, transaction_version);
-CREATE INDEX sender_all_pool_hstry_idx ON liquidity_events (sender, transaction_version);
-CREATE INDEX sender_all_mkt_rgstr_hstry_idx ON market_registration_events (sender, transaction_version);
+CREATE INDEX sender_all_swap_hstry_idx
+ON swap_events (
+    sender,
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    market_id, -- here.
+    symbol_emojis,
+    is_sell,
+    base_volume,
+    quote_volume,
+    avg_execution_price_q64,
+    in_bonding_curve
+);
+
+CREATE INDEX sender_mkt_swap_hstry_idx
+ON swap_events (
+    sender,
+    market_id, -- here.
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    symbol_emojis,
+    is_sell,
+    base_volume,
+    quote_volume,
+    avg_execution_price_q64,
+    in_bonding_curve 
+);
+
+CREATE INDEX sender_all_chat_hstry_idx
+ON chat_events (
+    sender,
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    market_id, -- here.
+    symbol_emojis,
+    "message",
+    user_emojicoin_balance,
+    last_swap_avg_execution_price_q64,
+    in_bonding_curve
+);
+
+CREATE INDEX sender_mkt_chat_hstry_idx
+ON chat_events (
+    sender,
+    market_id, -- here.
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    symbol_emojis,
+    "message",
+    user_emojicoin_balance,
+    last_swap_avg_execution_price_q64,
+    in_bonding_curve
+);
+
+CREATE INDEX sender_all_pool_hstry_idx
+ON liquidity_events (
+    sender,
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    market_id, -- here.
+    symbol_emojis,
+    liquidity_provided,
+    base_amount,
+    quote_amount,
+    lp_coin_amount,
+    last_swap_avg_execution_price_q64
+);
+
+CREATE INDEX sender_mkt_pool_hstry_idx
+ON liquidity_events (
+    sender,
+    market_id, -- here.
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    symbol_emojis,
+    liquidity_provided,
+    base_amount,
+    quote_amount,
+    lp_coin_amount,
+    last_swap_avg_execution_price_q64
+);
+
+-- No need to make two indexes- these are inherently unique for each market.
+CREATE INDEX sender_mkt_rgstr_hstry_idx
+ON market_registration_events (
+    sender,
+    transaction_version,
+    event_index
+) INCLUDE (
+    transaction_timestamp,
+    market_id, -- here.
+    symbol_emojis,
+    market_id
+);
