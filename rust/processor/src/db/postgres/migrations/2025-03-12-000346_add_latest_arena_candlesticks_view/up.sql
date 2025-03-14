8be17c35282b375ac7ba142ebf37e414f3aaf55b
Matt <90358481+xbtmatt@users.noreply.github.com>
2025-03-13 19:08:22 -0700

    Remove the view because it's not necessary

--- rust/processor/src/db/postgres/migrations/2025-03-12-000346_add_latest_arena_candlesticks_view/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-
--- The latest arena candlesticks, distinct on each `period`.
-CREATE VIEW arena_latest_candlesticks AS (
-    SELECT DISTINCT ON (melee_id, period) *
-    FROM arena_candlesticks
-    ORDER BY melee_id, period, start_time DESC
-);
